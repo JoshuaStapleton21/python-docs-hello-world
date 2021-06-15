c5e5de6f4082b935e9cb05b9a555bf6bb3adf113
“Joshua-Stapleton” <joshua.stapleton.ai@gmail.com>
2021-06-15 23:05:32 +0200

    minor changes

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, redirect, url_for, request
-# Route for handling the login page logic
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def home():
-    return render_template("home.html")
+@app.route("/")
+def hello():
+    return render_template('home.html')
+    
