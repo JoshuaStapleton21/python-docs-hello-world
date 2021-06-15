cfd13ff60357cd1e225de12309b17f5292b3dfe8
“Joshua-Stapleton” <joshua.stapleton.ai@gmail.com>
2021-06-15 23:47:22 +0200

    trying to load assets in-script

--- app.py
+++ app.py
@@ -2,35 +2,23 @@ from flask import Flask, render_template, redirect, url_for, request
 from flask.ext.assets import Bundle, Environment
 from .. import app
 
-# bundles = {
+bundles = {
 
-#     'home_js': Bundle(
-#         'js/lib/jquery-1.10.2.js',
-#         'js/home.js',
-#         output='gen/home.js'),
+    'home_js': Bundle(
+        'js/bootsnav.js',
+        'js/jquery.easypiechart.min.js',
+        output='gen/home.js'),
 
-#     'home_css': Bundle(
-#         'css/lib/reset.css',
-#         'css/common.css',
-#         'css/home.css',
-#         output='gen/home.css'),
+    'home_css': Bundle(
+        'css/animate.css',
+        'css/bootsnav.css',
+        'css/bootstrap.css',
+        output='gen/home.css'),
+}
 
-#     'admin_js': Bundle(
-#         'js/lib/jquery-1.10.2.js',
-#         'js/lib/Chart.js',
-#         'js/admin.js',
-#         output='gen/admin.js'),
+assets = Environment(app)
 
-#     'admin_css': Bundle(
-#         'css/lib/reset.css',
-#         'css/common.css',
-#         'css/admin.css',
-#         output='gen/admin.css')
-# }
-
-# assets = Environment(app)
-
-# assets.register(bundles)
+assets.register(bundles)
 app = Flask(__name__)
 
 @app.route("/")
