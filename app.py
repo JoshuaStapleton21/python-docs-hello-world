3927ab89f91d57de63191e98986bcfa7237dbdc3
“Joshua-Stapleton” <joshua.stapleton.ai@gmail.com>
2021-06-15 23:59:14 +0200

    removed bundling packages

--- app.py
+++ app.py
@@ -1,24 +1,5 @@
 from flask import Flask, render_template, redirect, url_for, request
-from flask.ext.assets import Bundle, Environment
-from .. import app
 
-bundles = {
-
-    'home_js': Bundle(
-        'js/bootsnav.js',
-        'js/jquery.easypiechart.min.js',
-        output='gen/home.js'),
-
-    'home_css': Bundle(
-        'css/animate.css',
-        'css/bootsnav.css',
-        'css/bootstrap.css',
-        output='gen/home.css'),
-}
-
-assets = Environment(app)
-
-assets.register(bundles)
 app = Flask(__name__)
 
 @app.route("/")
