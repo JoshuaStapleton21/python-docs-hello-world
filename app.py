4b501eba68278388f49976cc85f9b8964ffe0a53
JoshuaStapleton21 <jstapleton17@apu.edu>
2021-06-27 15:33:36 +0200

    added blog website to creative designers for multi-purpose functionality

--- app.py
+++ app.py
@@ -1,14 +1,33 @@
+# from flask import Flask, render_template
+
+# app = Flask(__name__)
+
+# @app.route('/')
+# def home():
+#     return render_template('creative_designers.html')
+
+# @app.route('/about/')
+# def about():
+#     return render_template('about.html')
+    
+# if __name__ == '__main__':
+# 	app.run(debug=True)
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('creative_designers.html')
+    return render_template('index.html')
 
-@app.route('/about/')
-def about():
-    return render_template('about.html')
+@app.route('/hello.html')
+def hello():
+    return render_template('hello.html')
+
+@app.route('/hoi.html')
+def skee():
+    return render_template('templates/2020/10/24/hoi.html')
     
 if __name__ == '__main__':
 	app.run(debug=True)
